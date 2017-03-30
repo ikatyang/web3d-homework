4cfb1a30f3eeb9db6b2f49a2661e1a9ad83205ba
ikatyang <ikatyang@gmail.com>
2017-03-31 01:10:17 +0800

    Update hw2 add orbit controls

--- src/homeworks/hw2/index.js
+++ src/homeworks/hw2/index.js
@@ -1,10 +1,11 @@
-import { camera, scene, init, animate } from '../../templates/three3D';
+import { camera as orbitCamera, scene, init, controls as orbitControls, renderer } from '../../templates/three3D';
 import House from './House';
 import roomConfigs from './rooms';
 
 const gcontrols = {
   speed: 80,
   blockedHeight: 10,
+  controls: 'PointerLock',
 };
 
 const clickDistance = 80;
@@ -18,17 +19,32 @@ const pointerRaycaster = new THREE.Raycaster(
 const collisionRaycaster = new THREE.Raycaster(
   undefined, undefined, undefined, collisionDistance);
 
+const pointerLockCamera = orbitCamera.clone();
+
 const gui = new dat.GUI();
 gui.close();
 gui.add(gcontrols, 'speed', 0, 200);
 gui.add(gcontrols, 'blockedHeight', 0, 80);
 gui.add(pointerRaycaster, 'far', 0, 200).name('clickDistance');
 gui.add(collisionRaycaster, 'far', 0, 50).name('collisionDistance');
+gui.add(gcontrols, 'controls', ['Orbit', 'PointerLock']).onChange((value) => {
+  const blocker = document.getElementById('blocker');
+  const icons = document.getElementById('cursor-icons');
+  if (value === 'Orbit') {
+    document.exitPointerLock();
+    blocker.style.display = 'none';
+    icons.style.display = 'none';
+  } else {
+    blocker.style.display = '';
+    icons.style.display = '';
+  }
+});
 
 let house;
 let intersects;
 let pointerLockControls;
 let mouseEvent;
+let isFirst = true;
 
 const pressed = {
   w: false,
@@ -38,7 +54,10 @@ const pressed = {
 };
 
 init(() => {
-  pointerLockControls = new THREE.PointerLockControls(camera);
+  orbitCamera.position.set(0, 300, 600);
+  scene.add(orbitCamera);
+
+  pointerLockControls = new THREE.PointerLockControls(pointerLockCamera);
   pointerLockControls.getObject().position.copy(origin);
   scene.add(pointerLockControls.getObject());
 
@@ -66,6 +85,7 @@ init(() => {
 
   document.addEventListener('pointerlockchange', () => {
     if (document.pointerLockElement === document.body) {
+      isFirst = false;
       pointerLockControls.enabled = true;
       blocker.style.display = 'none';
     } else {
@@ -106,56 +126,63 @@ init(() => {
   document.addEventListener('keyup', createKeyEvent(false), false);
 });
 
-animate(() => {
+(function animate() {
   const delta = clock.getDelta();
-  if (pointerLockControls.enabled) {
-    const pointerLockObject = pointerLockControls.getObject();
+  if (!isFirst && gcontrols.controls === 'PointerLock') {
+    if (pointerLockControls.enabled) {
+      const pointerLockObject = pointerLockControls.getObject();
 
-    const direction = new THREE.Vector3();
-    if (pressed.w) {
-      direction.z -= 1;
-    }
-    if (pressed.a) {
-      direction.x -= 1;
-    }
-    if (pressed.s) {
-      direction.z += 1;
-    }
-    if (pressed.d) {
-      direction.x += 1;
-    }
-    direction.normalize();
-
-    let blocked = false;
-
-    const collisionOrigin = pointerLockObject.position.clone().setY(gcontrols.blockedHeight);
-    const collisionDirection = pointerLockObject.localToWorld(direction.clone())
-      .sub(pointerLockObject.localToWorld(new THREE.Vector3()))
-      .normalize();
-    collisionRaycaster.set(collisionOrigin, collisionDirection);
-    if (collisionRaycaster.intersectObjects(house.borders).length > 0) {
-      blocked = true;
-    }
-
-    if (!blocked) {
-      pointerLockObject.translateOnAxis(direction, gcontrols.speed * delta);
-    }
-
-    if (mouseEvent) {
-      pointerRaycaster.setFromCamera(new THREE.Vector2(
-        ((mouseEvent.clientX / window.innerWidth) * 2) - 1,
-        ((mouseEvent.clientY / window.innerHeight) * -2) + 1,
-      ), camera);
-
-      intersects = pointerRaycaster.intersectObjects(house.borders);
-
-      if (intersects.length > 0 && house.doors.indexOf(intersects[0].object) === -1) {
-        intersects = [];
+      const direction = new THREE.Vector3();
+      if (pressed.w) {
+        direction.z -= 1;
       }
-    }
+      if (pressed.a) {
+        direction.x -= 1;
+      }
+      if (pressed.s) {
+        direction.z += 1;
+      }
+      if (pressed.d) {
+        direction.x += 1;
+      }
+      direction.normalize();
 
-    const isPointed = (intersects.length > 0);
-    document.getElementById('pointer').style.opacity = isPointed ? '1' : '0';
-    document.getElementById('blocked').style.opacity = blocked && !isPointed ? '1' : '0';
+      let blocked = false;
+
+      const collisionOrigin = pointerLockObject.position.clone().setY(gcontrols.blockedHeight);
+      const collisionDirection = pointerLockObject.localToWorld(direction.clone())
+        .sub(pointerLockObject.localToWorld(new THREE.Vector3()))
+        .normalize();
+      collisionRaycaster.set(collisionOrigin, collisionDirection);
+      if (collisionRaycaster.intersectObjects(house.borders).length > 0) {
+        blocked = true;
+      }
+
+      if (!blocked) {
+        pointerLockObject.translateOnAxis(direction, gcontrols.speed * delta);
+      }
+
+      if (mouseEvent) {
+        pointerRaycaster.setFromCamera(new THREE.Vector2(
+          ((mouseEvent.clientX / window.innerWidth) * 2) - 1,
+          ((mouseEvent.clientY / window.innerHeight) * -2) + 1,
+        ), pointerLockCamera);
+
+        intersects = pointerRaycaster.intersectObjects(house.borders);
+
+        if (intersects.length > 0 && house.doors.indexOf(intersects[0].object) === -1) {
+          intersects = [];
+        }
+      }
+
+      const isPointed = (intersects.length > 0);
+      document.getElementById('pointer').style.opacity = isPointed ? '1' : '0';
+      document.getElementById('blocked').style.opacity = blocked && !isPointed ? '1' : '0';
+    }
+    renderer.render(scene, pointerLockCamera);
+  } else {
+    orbitControls.update();
+    renderer.render(scene, orbitCamera);
   }
-});
+  requestAnimationFrame(animate);
+}());
